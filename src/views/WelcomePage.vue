9e04e371ab304b3b880ea8cef97ef3515bddad99
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-26 13:59:10 +0900

    computedプロパティを使ってみる

--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -3,6 +3,7 @@
     <h1>{{ title }}</h1>
     <p v-if="isEnabled">{{ subtitle }}</p>
     <button @dblclick="toggle">トグルする</button>
+    <p>{{ text }}</p>
   </div>
 </template>
 
@@ -13,6 +14,15 @@ export default {
       title: '初めてのVue.jsアプリです!',
       subtitle: 'ようこそ',
       isEnabled: true
+    }
+  },
+  computed: {
+    text() {
+      if(this.isEnabled) {
+        return 'こんにちは！'
+      } else {
+        return 'さよなら！' 
+      }
     }
   },
   methods: {
