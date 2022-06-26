eec7db552b12c38119c9a53f1609190355cdd145
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-26 13:47:08 +0900

    クリックイベントとメソッドオプションを使ってみる

--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>{{ title }}</h1>
     <p v-if="isEnabled">{{ subtitle }}</p>
+    <button v-on:click="toggle">トグルする</button>
   </div>
 </template>
 
@@ -12,6 +13,12 @@ export default {
       title: '初めてのVue.jsアプリです!',
       subtitle: 'ようこそ',
       isEnabled: true
+    }
+  },
+  methods: {
+    toggle () {
+      // メソッドプロパティ内でデータプロパティ呼び出すには、thisをつける
+      this.isEnabled = !this.isEnabled
     }
   }
 }
