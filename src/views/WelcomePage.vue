4b18d06bff50c24225011b18462823e55f43903e
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-26 12:07:55 +0900

    データプロパティを使ってみる

--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    こんにちは
+    <h1>{{ 3 + 3 }}</h1>
+    <p>{{ subtitle }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '初めてのVue.jsアプリです!',
+      subtitle: 'ようこそ'
+    }
+  }
+}
+</script>
