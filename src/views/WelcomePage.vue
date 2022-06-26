50191ffbb7a78e04ba5b46179a67318f87e7d80e
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-26 15:47:32 +0900

    グローバルCSSを適用する

--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -1,50 +1,15 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <!-- TestComponentのHTMLが出力される -->
-    <!-- 子コンポーネントで$emitメソッドが実行された場合に、親コンポーネントのtoggleメソッドを実行する -->
-    <TestComponent @toggle="toggle" message="コンポーネントにデータが渡されています"/>
-    <p v-if="isEnabled">こんにちは</p>
+  <div class="container welcome">
+    <p>ようこそ！</p>
   </div>
 </template>
 
 <script>
-// TestComponentの読み込み
-import TestComponent from '../components/TestComponent.vue'
-
-export default {
-  // 読み込んだコンポーネントの定義
-  components: {
-    TestComponent
-  },
-  data() {
-    return {
-      title: '初めてのVue.jsアプリです!',
-      subtitle: 'ようこそ',
-      isEnabled: true
-    }
-  },
-  computed: {
-    text() {
-      if(this.isEnabled) {
-        return 'こんにちは！'
-      } else {
-        return 'さよなら！' 
-      }
-    }
-  },
-  methods: {
-    toggle () {
-      // メソッドプロパティ内でデータプロパティ呼び出すには、thisをつける
-      this.isEnabled = !this.isEnabled
-    }
-  }
-}
 </script>
 
 <style scoped>
-p {
-  font-size: 20px;
-  color: red;
+.welcome {
+  text-align: center;
+  padding: 20px 0;
 }
 </style>
