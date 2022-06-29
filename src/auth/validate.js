bdb5ca82f917f3a63c8b7cfaa28e87fbc69e2301
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-29 19:40:03 +0900

    ログイン状態によるアクセス制御

--- src/auth/validate.js
+++ src/auth/validate.js
@@ -1,25 +1,44 @@
-import axios from 'axious'
+import axios from 'axios'
+import { ref } from 'vue'
 
-const validate = async() => {
+const error = ref(null)
+
+const validate = async () => {
+  error.value = null
+
   const uid = window.localStorage.getItem('uid')
   const client = window.localStorage.getItem('client')
   const accessToken = window.localStorage.getItem('access-token')
 
   try {
+    // railsとapi通信
     const res = await axios.get('http://localhost:3000/auth/validate_token', {
       headers: {
         uid: uid,
-        'access-token': accessToken,
-        client: client
+        "access-token": accessToken,
+        client: client,
       }
     })
-    
-    return res
-  } catch(err) {
-    console.log(err)
-  }
 
-  const useValidate = () => {
-    return { validate }
+    if (!res) {
+      throw new Error('認証に失敗しました')
+    }
+
+    error.value = null
+
+    return res
+  } catch (err) {
+    error.value = '認証に失敗しました'
+
+    window.localStorage.removeItem('uid')
+    window.localStorage.removeItem('access-token')
+    window.localStorage.removeItem('client')
+    window.localStorage.removeItem('name')
   }
 }
+
+const useValidate = () => {
+  return { error, validate }
+}
+
+export default useValidate
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,62 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomePage from '../views/WelcomePage'
 import ChatroomPage from '../views/ChatroomPage'
+// validate.jsの読み込み
+import useValidate from '../auth/validate'
+// validateメソッドの定義
+const { error, validate } = useValidate()
 
+// eslint-disable-next-line no-unused-vars
 // ナビゲーションガードの設定
-const requireAuth = async(to, from, next) => {
-  console.log('requireAuthが呼ばれています!')
-  
-  next()
+const requireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  // uid、client、accessTokenのいずれかの値がnullだった場合は、ウェルカムページにリダイレクト
+  if (!uid || !client || !accessToken) {
+    console.log("ログインしていません")
+    next({ name: 'Welcome' })
+    // ここで処理を終わらせてので、return
+    return
+  }
+
+  await validate()
+  // チャットルームページへリダイレクト
+  if (error.value) {
+    console.log("認証に失敗しました")
+    next({ name: 'Welcome' })
+  } else {
+    next()
+  }
 }
+
+const noRequireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid && !client && !accessToken) {
+    next()
+    return
+  }
+
+  await validate()
+
+  if (!error.value) {
+    next({ name: 'Chatroom' })
+  } else {
+    next()
+  }
+}
+
 
 const routes = [
   {
     path: '/',
     name: 'Welcome',
-    component: WelcomePage
+    component: WelcomePage,
+    beforeEnter: noRequireAuth
   },
   {
     path: '/chatroom',
@@ -23,6 +66,7 @@ const routes = [
     beforeEnter: requireAuth
   }
 ]
+
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
