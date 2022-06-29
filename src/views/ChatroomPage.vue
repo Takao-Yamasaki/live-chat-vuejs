95768fde4db7206b5c790d3b7a5b559c2fd61b09
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-29 19:50:23 +0900

    コンポーネントを作成

--- src/views/ChatroomPage.vue
+++ src/views/ChatroomPage.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="container">
-    <Navbar/>  
+    <Navbar/>
+    <ChatWindow/>
   </div>
 </template>
 
 <script>
 import Navbar from '../components/NavbarPage.vue'
+import ChatWindow from '../components/ChatWindow.vue'
 
 export default {
-  components: { Navbar },
+  components: { Navbar, ChatWindow }
 }
 </script>
 
